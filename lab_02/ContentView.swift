9cbd166e0304a7b852b0586fa3cdc41ea67c349e
Arch Patel <73293199+archp4@users.noreply.github.com>
2025-05-16 18:01:27 -0400

    Initial Commit

---
+++ lab_02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab_02
+//
+//  Created by Arch Umeshbhai Patel on 2025-05-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab_02/lab_02App.swift
@@ -0,0 +1,17 @@
+//
+//  lab_02App.swift
+//  lab_02
+//
+//  Created by Arch Umeshbhai Patel on 2025-05-16.
+//
+
+import SwiftUI
+
+@main
+struct lab_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
